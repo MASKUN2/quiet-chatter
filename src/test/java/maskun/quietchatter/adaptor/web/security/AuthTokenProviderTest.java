95f28faad4d37ce877b8f03cb10b9e605d05a8ba
MASKUN2 <maskunflower2@naver.com>
2025-11-19 17:10:53 +0900

    fix: Guest Auth 발급 테스트 버그 수정

--- src/test/java/maskun/quietchatter/adaptor/web/security/AuthTokenProviderTest.java
+++ src/test/java/maskun/quietchatter/adaptor/web/security/AuthTokenProviderTest.java
@@ -1,11 +1,14 @@
 package maskun.quietchatter.adaptor.web.security;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.instancio.Select.field;
 import static org.mockito.Mockito.when;
 
 import maskun.quietchatter.hexagon.domain.member.Member;
+import maskun.quietchatter.hexagon.domain.member.Role;
 import maskun.quietchatter.hexagon.inbound.GuestRegistrable;
 import org.instancio.Instancio;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -15,14 +18,20 @@ import org.springframework.security.core.Authentication;
 @ExtendWith(MockitoExtension.class)
 class AuthTokenProviderTest {
 
+    private AuthTokenProvider authTokenProvider;
+
     @Mock
     private GuestRegistrable guestRegistrable;
 
+    @BeforeEach
+    void setUp() {
+        when(guestRegistrable.createNewGuest()).thenReturn(getGuestFixture());
+
+        authTokenProvider = new AuthTokenProvider(guestRegistrable);
+    }
+
     @Test
     void getGuest() {
-        when(guestRegistrable.createNewGuest()).thenReturn(Instancio.create(Member.class));
-
-        AuthTokenProvider authTokenProvider = new AuthTokenProvider(guestRegistrable);
         Authentication auth = authTokenProvider.getGuest();
 
         assertThat(auth).isNotNull();
@@ -32,5 +41,11 @@ class AuthTokenProviderTest {
         assertThat(auth.getAuthorities().size()).isEqualTo(1);
         assertThat(auth.getAuthorities().iterator().next().getAuthority()).isEqualTo("ROLE_GUEST");
 
+    }
+
+    private static Member getGuestFixture() {
+        return Instancio.of(Member.class)
+                .set(field(Member::getRole), Role.GUEST)
+                .create();
     }
 }
