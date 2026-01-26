63ae1b3b3e1497190036d3307e5d42416747947c
MASKUN2 <maskunflower2@naver.com>
2026-01-26 22:42:20 +0900

    test: migrate `TalkQueryServiceTest` to use MockitoJUnit runner

--- src/test/java/maskun/quietchatter/talk/application/TalkQueryServiceTest.java
+++ src/test/java/maskun/quietchatter/talk/application/TalkQueryServiceTest.java
@@ -1,8 +1,8 @@
 package maskun.quietchatter.talk.application;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.util.UUID;
 import maskun.quietchatter.talk.application.in.TalkQueryRequest;
@@ -10,27 +10,23 @@ import maskun.quietchatter.talk.application.out.TalkRepository;
 import maskun.quietchatter.talk.domain.Talk;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 class TalkQueryServiceTest {
-    @MockitoBean
+    @Mock
     private TalkRepository talkRepository;
-
-    @Autowired
-    private TalkQueryService talkQueryService;
 
     @Test
     @DisplayName("없는 책을 조회시 빈 페이지 반환")
     void findByNotExistingBook() {
         when(talkRepository.findByBookIdOrderByCreatedAtDesc(any(), any())).thenReturn(Page.empty());
 
+        TalkQueryService talkQueryService = new TalkQueryService(talkRepository, null);
         TalkQueryRequest request = new TalkQueryRequest(UUID.randomUUID(), PageRequest.of(0, 10));
         Page<Talk> page = talkQueryService.findBy(request);
 
